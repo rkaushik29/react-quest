89170a292242d713dd6fd3a2ea4a6e51a29ba1b1
r3kaushi <r3kaushi@uwaterloo.ca>
2024-05-11 14:41:29 -0400

    api req table

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ApplicationCard from './Components/ApplicantStatus';
+import RequestLog from './Components/ApiRequestTable';
 
 function App() {
   return (
     <div className="h-screen flex items-center justify-center">
-      <ApplicationCard />
+      <RequestLog />
     </div>
   );
 }
